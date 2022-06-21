29ed263b8c1170f7da619b86a0897af1af0f943d
Chad Tarpey <chadtarpey@gmail.com>
2022-06-21 17:32:23 -0500

    created post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
 
+  resources :posts do
+    collection do
+      get 'hobby'
+      get 'study'
+      get 'team'
+    end
+  end
   root to: 'pages#index'
 
   devise_scope :user do
@@ -10,4 +17,6 @@ Rails.application.routes.draw do
  devise_scope :user do
   get 'signup', to: 'devise/registrations#new'
 end
+
+
 end
